504e1c5280a91a1662e5d5e124afff0155bf2447
Parisito1234 <58274947+Parisito1234@users.noreply.github.com>
2021-03-17 13:32:20 -0400

    Update cc_roadmap.go
    bungie url change

--- cc_references/cc_roadmap.go
+++ cc_references/cc_roadmap.go
@@ -1,4 +1,4 @@
-{{ $imageurl:= "https://www.bungie.net/pubassets/pkgs/148/148944/ae_season13_cal_EN.jpg?cv=3983621215&av=2328450057" }}
+{{ $imageurl:= "https://www.bungie.net/pubassets/pkgs/149/149495/ae_season13_cal_EN_1.jpg" }}
 {{ $avatar := "https://www.bungie.net/common/destiny2_content/icons/f9ad6ac2aecaa89b2b4e075cc6e8b89f.png" }}
 {{$embed := cembed 
     "color" 1772743
