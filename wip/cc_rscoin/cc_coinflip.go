49ea90dff5d3391a31326ccd5baa25d7ffb2d7cb
Parisito <58274947+Parisito1234@users.noreply.github.com>
2021-08-25 20:09:19 -0400

    Update cc_coinflip.go

--- wip/cc_rscoin/cc_coinflip.go
+++ wip/cc_rscoin/cc_coinflip.go
@@ -37,7 +37,7 @@
       {{sendMessage nil (joinStr "" "The coin landed on " $rollType " and " .User.Username " has won `" $amount "`!!!\n" $.User.Username " now has `" (add $curBalance $amount) "` " $e)}}
       {{dbSet $.User.ID $key (toString (add $curBalance $amount))}}
     {{else if eq $winState 2}}
-      {{dbSet $.User.ID $key (toString (sub $curBalance $amount))}}
+      {{dbSet $.User.ID $key (toString (sub $curBalance (div $amount 2)))}}
       {{$amount = mult $amount 10}}
       {{sendMessage nil (joinStr "" "Nope! It was " $rollType "! To the thrallpit with you!\n*Muted for " $amount " minutes*")}}
       {{$a := execAdmin "mute" $.User (joinStr "" $amount "m") "coinflips"}}
